ab99bc9d8578ba8f9407e3008560c61f814d8694
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2023-02-14 21:50:18 -0300

    feat: creating the first routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/courses", (request, response) => {
   const { name } = request.body;
   return response.json({
